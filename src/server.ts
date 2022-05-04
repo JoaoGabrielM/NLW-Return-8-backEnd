5fa12a27d9d73f31595a2c2b39a066124b170708
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-04 16:28:33 -0300

    Criando a rota do feedback e aceitando requisição em json

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', (request, response) => {
+    console.log(request.body);
+
+    return response.send("Hello World");
+})
 
 app.listen(3333, () => {
     console.log("HTTP server running!");
